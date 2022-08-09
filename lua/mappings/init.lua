842cc478c656b04c89f027c4a73ea82eeccba71b
StoneHappy <18017158729@163.com>
2022-08-10 00:36:19 +0800

    feate: add toggleTermm

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -7,3 +7,18 @@ map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
 map('n', '<leader>1', ':tabnext<CR>', opts)
 map('n', '<leader>2', ':tabprevious<CR>', opts)
 map('n', '<leader>3', ':tabclose<CR>', opts)
+
+-- toggleterm.nvim mapping
+map('n', '<leader>t', ':ToggleTerm<CR>', opts)
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
